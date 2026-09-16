1d5cf171f3bd2d2a48faec3d78f418611514d512
Petar Baskovic <5arknopfler@gmail.com>
2026-09-17 00:14:11 +0200

    Migrations Faza 3

--- backend/Wediplan.Api/Migrations/20260916220547_Faza3Auth.Designer.cs
+++ backend/Wediplan.Api/Migrations/20260916220547_Faza3Auth.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using NpgsqlTypes;
@@ -13,9 +14,11 @@ using Wediplan.Api.Data;
 namespace Wediplan.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260916220547_Faza3Auth")]
+    partial class Faza3Auth
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
