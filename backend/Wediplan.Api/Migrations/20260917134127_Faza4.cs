5cafc37ccfec1a7caec357edad4bcbf87bfc65dd
Petar Baskovic <5arknopfler@gmail.com>
2026-09-17 16:25:57 +0200

    migrations faza4

---
+++ backend/Wediplan.Api/Migrations/20260917134127_Faza4.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wediplan.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Faza4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
